6ed86efd71c03f1fd4441fdf32396c9d0b758b96
Sam Fritton <Samuel.Fritton@target.com>
2021-02-19 14:48:04 -0600

    home page styles

--- src/components/Wave/index.tsx
+++ src/components/Wave/index.tsx
@@ -4,11 +4,13 @@ import { cssCustomProperties } from '../../util';
 
 const Wave: React.FC = () => {
   return (
-    <div className={styles.waveWrapper}>
-      <div className={styles.wave}></div>
-      <div className={styles.wave} style={cssCustomProperties({ '--n': 1 })}></div>
-      <div className={styles.wave} style={cssCustomProperties({ '--n': 2 })}></div>
-      <div className={styles.wave} style={cssCustomProperties({ '--n': 3 })}></div>
+    <div className={styles.msBorder}>
+      <div className={styles.waveWrapper}>
+        <div className={styles.wave}></div>
+        <div className={styles.wave} style={cssCustomProperties({ '--n': 1 })}></div>
+        <div className={styles.wave} style={cssCustomProperties({ '--n': 2 })}></div>
+        <div className={styles.wave} style={cssCustomProperties({ '--n': 3 })}></div>
+      </div>
     </div>
   );
 };
