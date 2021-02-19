ad6dd92b13a7806db265305a1a25b344293d1bed
Sam Fritton <Samuel.Fritton@target.com>
2021-02-19 13:39:10 -0600

    tech stack changes

--- src/components/TechStack/techs/TechTypescript.tsx
+++ src/components/TechStack/techs/TechTypescript.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
-import { Tech } from '..';
+import styles from '../techstack.module.css';
 
-const TechTypescript: React.FC = () => <Tech src="/images/typescript.png">Typescript</Tech>;
+const TechTypescript: React.FC = () => {
+  return (
+    <li className={styles.tech}>
+      <img
+        src="/images/typescript.png"
+        className={`${styles.techImage} ${styles.tsImage}`}
+        alt=""
+      />
+      Typescript
+    </li>
+  );
+};
 
 export default TechTypescript;
