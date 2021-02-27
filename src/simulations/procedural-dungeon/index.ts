717a9c4e478393e594ecb72bff582427d03b61a6
Sam Fritton <sfritton94@gmail.com>
2021-02-27 14:32:35 -0600

    adding documentation

--- src/simulations/procedural-dungeon/index.ts
+++ src/simulations/procedural-dungeon/index.ts
@@ -3,7 +3,7 @@ import { ProceduralDungeon } from './ProceduralDungeon';
 
 export const proceduralDungeon = (p5: P5) => {
   const dungeon = new ProceduralDungeon(p5);
-  const width = Math.min(document.body.clientWidth - 100, 1000);
+  const width = Math.min(document.body.clientWidth - 100, 700);
   // if the width is less than 662, assume we're on a vertical screen
   const height = width < 662 ? (width * 4) / 3 : (width * 3) / 4;
 
