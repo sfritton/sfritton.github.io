ea5e85f8deea13fecb44cff711dc84a99a5e4e1a
Sam Fritton <Samuel.Fritton@target.com>
2021-04-30 10:47:51 -0500

    adding commentary to projects

--- src/pages/projects/simulations/gravity.tsx
+++ src/pages/projects/simulations/gravity.tsx
@@ -13,13 +13,20 @@ const GravityPage: React.FC<PageProps> = () => {
     <Layout title="Gravity Simulation">
       <h1>Gravity Simulation</h1>
       <Simulation sketch={gravity(selectRef)} />
-      <Select label="Scenarios" ref={selectRef}>
+      <Select label="Scenario" ref={selectRef}>
         {allScenarios.map(({ name }) => (
           <option key={name} value={name}>
             {name}
           </option>
         ))}
       </Select>
+      <div style={{ marginTop: 'var(--space-lg)' }}>
+        This system takes a set of planets and simulates their interactions. Each planet has a mass,
+        an initial position, and an initial velocity. From there, the system simulates physical
+        forces from gravity to determine the planets' movement. The orbits you see are not
+        pre-programmed, they arise the same way they do in real life: a balance between the planet's
+        momentum and the force of gravity from a larger body.
+      </div>
     </Layout>
   );
 };
